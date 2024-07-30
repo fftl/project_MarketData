8b3d6d1cbbf3254b7d63e1aae45da234b9ff6563
이동민 <fftl0785@gmail.com>
2024-07-30 18:23:20 +0900

    update : project init

---
+++ src/test/java/site/fftl/marketdata/MarketdataApplicationTests.java
@@ -0,0 +1,13 @@
+package site.fftl.marketdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
